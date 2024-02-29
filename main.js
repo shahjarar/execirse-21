433bf263bdb9641921520e049e3ed5e705257387
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 17:57:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//Task 21
+// think of something you store in a Typescrit Object.write a program
+//that creates Objects containing these items.
+var person = { name: "Mahik", fname: "female", age: 27 };
+console.log(person);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//Task 21
+// think of something you store in a Typescrit Object.write a program
+//that creates Objects containing these items.
+
+let person : {name :string, fname: string , age : number} ={name: "Mahik", fname:"female",age: 27}
+console.log(person)
